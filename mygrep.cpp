6eac87da2abc884cc78be5dafe1c4950b3bdf5e2
Kristian <kristian.sikanen@hotmail.com>
2023-02-13 21:58:32 +0200

    increment 2 done

--- mygrep.cpp
+++ mygrep.cpp
@@ -5,57 +5,107 @@
 using namespace std;
 
 void increment1();
+void increment2(int, char *[]);
+// void increment3(int, char *[])
 
 int main(int argc, char *argv[])
 {
-    if (argc <= 1 || argc >= 3)
+
+    if (argc == 1)
     {
         increment1();
-        break;
     }
 
-    if (argv[1] == "following")
+    if (argc == 3)
     {
-        ifstream file(argv[2]);
-        if (!file)
-        {
-            cerr << "Error opening file" << endl;
-            return 1;
-        }
-        if (file)
-        {
-            // getline.
-        }
-        
+        increment2(argc, argv);
     }
+
+    if (argc == 4)
+    {
+        true;
+    }
+    
+    // Lukee argc, jos cmd- syote yli 3 argumenttia pitkä, tulostetaan ohje
+    if (argc > 4 || argc == 2)
+    {
+        cerr << "\nUsage: mygrep keyword file\n";
+        cerr << "\noptional: mygrep ('options') keyword file";
+        cerr << "\n-olo -> Displays line numbers and number of occurences";
+        cerr << "\n-ol  -> Displays only line numbers";
+        cerr << "\n-oo  -> Displays only number of occurences\n";
+
+        return 1;
+    }
+
+    return 0;
 }
 
 void increment1()
 {
+    //*--------inkrementti 1----------*//
+
     string inString;
     string searchStr;
     int lapcount = 0;
 
-    cout << "Give a string from which to search for: ";
+    cout << "Give a string from which to search from: ";
     getline(cin, inString);
     cout << "Give search string:";
     getline(cin, searchStr);
 
+    // For- luuppi, joka luuppaa syotetyn string- muuttujan sisaltaman merkkimaaran verran
+    // Jos ensimmaisen merkin vastaavuus loytyy, alkaa muuttuja lapcount kaymaan lapi seuraavia tulevia merkkeja
+    // Kun taysi vastaavuus loydetty, nollataan lapcount tulevien vastaavuuksien varalle
     for (int i = 0; i < inString.size(); i++)
     {
+        if (inString[i] != searchStr[lapcount])
+        {
+            lapcount = 0;
+        }
 
         if (inString[i] == searchStr[lapcount])
         {
             lapcount++;
         }
+
         if (lapcount == searchStr.size())
         {
-            cout << "Key " << searchStr << " found at slot " << i << "\n";
+            cout << "Key " << searchStr << " found at slot " << (i - searchStr.size() + 1) << "\n";
             lapcount = 0;
         }
-        else
+    }
+}
+
+void increment2(int argc, char *argv[])
+{
+    //*--------inkrementti 2----------*//
+    // Luodaan muuttuja jolle sijoitetaan hakusana, avataan tiedosto ja luetaan sisalto
+    // Jos tiedostoa ei saada auki, palauttaa errorin
+    string keyword = argv[1];
+    ifstream file(argv[2]);
+    if (!file)
+    {
+        cerr << "Error opening file\n";
+    }
+
+    // Luodaan muuttuja johon sijoitetaan tiedoston tekstirivi
+    // Luupataan lopetusehdolla -> 'niin pitkaan kun tekstia loytyy'
+    // Jos ohjelma loytaa vastaavuuden, tulostetaan se naytolle
+    // Npos -> maksimikoko size_t:eelle -> size_t on sizeof- operaattorin palauttama tyyppi
+    //- toimii siis tarkastuksena tulostukselle
+    string line;
+    bool notFound = true;
+    while (getline(file, line))
+    {
+        if (line.find(keyword) != string::npos)
         {
-            cout << "\nKeyword not found in given string"
+            cout << line << endl;
+            notFound = false;
         }
+    }
+    if (notFound)
+    {
+        cout << "\nRequested keyword not found in this file\n";
     }
 }
